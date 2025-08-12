e4043e702b26b3a5b8179974af5ad819bbc13646
Rayyan <rk4004104@gmail.com>
2025-08-12 19:21:37 +0500

    starting

---
+++ hello folder/javascript/index.js
@@ -0,0 +1,19 @@
+let name ='Mehak';
+let age = 16;
+let isapproved = false;
+let firstname = undefined;
+let selectedcolor = null;
+
+function greet(name, lastname) {
+    console.log('Hello ' + name + ' ' + lastname);
+}
+
+greet ('Mehak', 'Herry');
+greet('Merry')
+
+
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2))
