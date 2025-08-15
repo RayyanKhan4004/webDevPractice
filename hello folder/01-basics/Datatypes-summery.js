8a0473b74f6668289ea052b3c5bbcc6936aa1376
Rayyan <rk4004104@gmail.com>
2025-08-15 16:22:41 +0500

    datatypes

---
+++ hello folder/01-basics/Datatypes-summery.js
@@ -0,0 +1,24 @@
+// primitive
+
+// 7 types : string, Number, Boolearn, Null, Undefined, Symbol,BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol('123')
+const anotherId = symbol ('123')
+
+console.log(id === anotherId);
+
+const bignumber = 34564342456443535n
+
+
+
+
+//Refrence (non primitive)
+
+//Array, objects, functions
