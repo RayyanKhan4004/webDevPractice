47c0c698ee93832c3635ddea3d2b4960489d02aa
Rayyan <rk4004104@gmail.com>
2025-08-16 17:53:20 +0500

    working

--- hello folder/01-basics/05_string.js
+++ hello folder/01-basics/05_string.js
@@ -1,7 +1,7 @@
 const name ="mehak"
 const repoCount = 50
 
-// console.log(name + repoCount + "value");
+// console.log(name + repoCount + "value"); ya use nhi karnaaaa
 
 console.log(`Hello my name is ${name} and my repo count is $
     {repoCount}`);
